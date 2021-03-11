ef6ade59bb7e1709d0fd6c39c2d81568e1a1f06b
dgalindol <dgalindol@iesch.org>
2021-03-11 16:46:46 +0100

    Agregar archivos de proyecto.

---
+++ Examen ED Diego Galindo/Circle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Examen_ED_Diego_Galindo
+{
+    public class Circle
+    {
+        public double Radius { get; set; }    
+        public double Area { 
+            get 
+            {
+                return (Radius * Radius) * Math.PI;
+            } 
+        }
+    }
+}
