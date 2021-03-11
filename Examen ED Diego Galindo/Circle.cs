2d775eafcce2c76884e4eb0fe762136f281eb14b
dgalindol <dgalindol@iesch.org>
2021-03-11 16:51:27 +0100

    Ejercicio 6

--- Examen ED Diego Galindo/Circle.cs
+++ Examen ED Diego Galindo/Circle.cs
@@ -4,12 +4,23 @@ namespace Examen_ED_Diego_Galindo
 {
     public class Circle
     {
-        public double Radius { get; set; }    
-        public double Area { 
-            get 
+        private double radius;
+
+        public double Radius { get => radius;
+            set
+            {
+                if (Radius < 0)
+                {
+                    throw new ArgumentException("El valor debe ser mayor que 0");
+                }
+            }
+        }
+        public double Area
+        {
+            get
             {
                 return (Radius * Radius) * Math.PI;
-            } 
+            }
         }
     }
 }
