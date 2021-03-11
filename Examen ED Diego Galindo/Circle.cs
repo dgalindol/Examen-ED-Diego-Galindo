2fe776bdeed1370bc0191984f6e2bc2e336c5272
dgalindol <dgalindol@iesch.org>
2021-03-11 17:00:38 +0100

    Ejercicio 11

--- Examen ED Diego Galindo/Circle.cs
+++ Examen ED Diego Galindo/Circle.cs
@@ -2,9 +2,18 @@
 
 namespace Examen_ED_Diego_Galindo
 {
+    /// <summary>
+    /// Esta clase va a calcular el Area de un circulo a partir de un Radio dado
+    /// </summary>
     public class Circle
     {
+        /// <summary>
+        /// Esta priedad va a dar el valor del Radio del circulo
+        /// /// </summary>
         public double Radius { get; set; }    
+        /// <summary>
+        /// Esta propiedad devuelve el valor del area del circulo calculada a traves de Radius
+        /// </summary>
         public double Area { 
             get 
             {
