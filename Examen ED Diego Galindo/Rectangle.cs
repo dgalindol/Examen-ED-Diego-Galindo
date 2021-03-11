d875cdd918cdc0f25c10d273ad6beaa7d896c459
dgalindol <dgalindol@iesch.org>
2021-03-11 18:24:32 +0100

    Ejercicio 16.2

--- Examen ED Diego Galindo/Rectangle.cs
+++ Examen ED Diego Galindo/Rectangle.cs
@@ -12,28 +12,43 @@ namespace Examen_ED_Diego_Galindo
         /// <summary>
         /// Comprueba que el valor Width es superior a 0
         /// </summary>
+        private static double width;
         public double Width
         {
             get => Width;
             set
             {
                 if (Width < 0) throw new ArgumentException("El valor debe ser superior a 0");
+                width = Width;
             }
         }
 
         /// <summary>
         /// Comprueba que el valor Height es superior a 0
         /// </summary>
-        public double Height { get => Height;
+        private static double height;
+        public double Height 
+        { 
+            get => Height;
             set
             {
                 if (Height < 0) throw new ArgumentException("El valor debe ser superior a 0");
+                height = Height;
             }
         }
 
+        public static double Area2(double ancho, double alto)
+        {
+            ancho = width;
+            alto = height;
+            double area3 = ancho * alto;
+            return area3; 
+        }
         /// <summary>
         /// Implementa la interfaz IArea
         /// </summary>
         public double Area => throw new NotImplementedException();
+
+        
     }
 }
--- Examen Test Diego Galindo/UnitTest1.cs
+++ Examen Test Diego Galindo/UnitTest1.cs
@@ -8,11 +8,17 @@ namespace Examen_Test_Diego_Galindo
     [TestClass]
     public class UnitTest1
     {
-        //[TestMethod]
-        //public void TestMethod1()
-        //{
-        //    double ancho = 5;
-        //    Assert.AreEqual(5, Rectangle.Width(ancho));
-        //}
+        [TestMethod]
+        [DataRow(5, 5)]
+        public void TestMethod1(double ancho, double alto)
+        {
+            Rectangle r = new Rectangle();
+            r.Height = alto;
+            r.Width = ancho;
+
+            double calc = r.Height * r.Width;
+
+            Assert.AreEqual(calc, Rectangle.Area2(ancho, alto));
+        }
     }
 }
